de3c91efbf62d91623b51b4e8a85b0850979953e
Myrmex <dfusi@hotmail.com>
2023-01-01 22:49:05 +0100

    Initial commit

---
+++ projects/myrmidon/cadmus-part-epigraphy-support/src/public-api.ts
@@ -0,0 +1,9 @@
+/*
+ * Public API Surface of cadmus-part-epigraphy-support
+ */
+
+export * from './lib/epi-support-part';
+export * from './lib/epi-support-part/epi-support-part.component';
+export * from './lib/epi-support-part-feature/epi-support-part-feature.component';
+
+export * from './lib/cadmus-part-epigraphy-support.module';
---
+++ projects/myrmidon/cadmus-part-epigraphy-writing/src/public-api.ts
@@ -0,0 +1,9 @@
+/*
+ * Public API Surface of cadmus-part-epigraphy-writing
+ */
+
+export * from './lib/epi-writing-part';
+export * from './lib/epi-writing-part/epi-writing-part.component';
+export * from './lib/epi-writing-part-feature/epi-writing-part-feature.component';
+
+export * from './lib/cadmus-part-epigraphy-writing.module';
