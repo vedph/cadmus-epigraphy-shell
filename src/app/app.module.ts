bfdcdd117ea658e0f28f67f81168f3ef367da8c9
Myrmex <dfusi@hotmail.com>
2023-01-02 22:31:50 +0100

    Removed legacy lib

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -38,7 +38,6 @@ import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatTreeModule } from '@angular/material/tree';
-import { FlexLayoutModule } from '@angular/flex-layout';
 
 // ELF
 import { devTools } from '@ngneat/elf-devtools';
@@ -154,7 +153,6 @@ export function initElfDevTools(actions: Actions) {
     MatTooltipModule,
     MatToolbarModule,
     MatTreeModule,
-    FlexLayoutModule,
     // vendors
     MonacoEditorModule.forRoot(),
     MarkdownModule.forRoot(),
