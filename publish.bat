2dcb5e2e70cfb75b8e173cfabc212bd401c374f7
Myrmex <dfusi@hotmail.com>
2023-01-03 21:31:32 +0100

    Publish

---
+++ publish.bat
@@ -0,0 +1,28 @@
+@echo off
+echo NPM PUBLISH
+echo Before continuing, ensure that:
+echo - you are logged in (npm whoami)
+echo - you have successfully rebuilt all the libraries (npm run...)
+pause
+
+cd .\dist\myrmidon\cadmus-fr-epigraphy-ligatures
+call npm publish --access=public
+cd ..\..\..
+pause
+
+cd .\dist\myrmidon\cadmus-part-epigraphy-pg
+call npm publish --access=public
+cd ..\..\..
+pause
+
+cd .\dist\myrmidon\cadmus-part-epigraphy-support
+call npm publish --access=public
+cd ..\..\..
+pause
+
+cd .\dist\myrmidon\cadmus-part-epigraphy-writing
+call npm publish --access=public
+cd ..\..\..
+pause
+
+echo ALL DONE
